12bdb1b3009775dde9478ca23c7725dd772369c7
joshtin2505 <justincastro2505@gmail.com>
2024-05-02 17:05:40 -0500

    move it to the correct package

---
+++ src/main/java/com/example/App/services/converters/ClienteConverter.java
@@ -0,0 +1,18 @@
+package com.example.App.services.converters;
+
+import com.example.App.DTO.ClienteDTO;
+import com.example.App.entities.Cliente;
+
+
+public class ClienteConverter {
+
+    public static ClienteDTO convertToDTO(Cliente cliente) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public static Cliente convertToEntity(ClienteDTO clienteDTO) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+}
+
