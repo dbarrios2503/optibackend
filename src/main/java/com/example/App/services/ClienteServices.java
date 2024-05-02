26e56c744dedc5c89b5e83071a0da40e751c160c
joshtin2505 <justincastro2505@gmail.com>
2024-05-02 17:06:55 -0500

    Refactor ClienteServices and add UsuarioService classes

--- src/main/java/com/example/App/services/ClienteServices.java
+++ src/main/java/com/example/App/services/ClienteServices.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.App.services;
 
 import com.example.App.DTO.ClienteDTO;
@@ -10,55 +6,41 @@ import com.example.App.repositories.ClienteRepository;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.transaction.Transactional;
+
+import com.example.App.services.converters.ClienteConverter;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- *
- * @author Daniel BC 2
- */
+@Service
 public class ClienteServices {
     @Autowired
- 
     private ClienteRepository clienteRepository ;
     
        public ClienteDTO getClienteDTO(Long id) {
-        // Recupera la entidad del repositorio
-        Cliente cliente = clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Producto no encontrado"));
-        // Convierte la entidad a DTO y devuelve
-         return ClienteConverter.convertToDTO(cliente );
+         return ClienteConverter.convertToDTO(clienteRepository.findById(id).orElseThrow(() -> new RuntimeException("Producto no encontrado")));
     }
        @GetMapping("/add")
-// Método para obtener todos los productos y devolver una lista de DTOs
     public List<ClienteDTO> getAllCliente() {
-        // Recupera todas las entidades del repositorio
         List<Cliente> products = clienteRepository.findAll();
-        // Convierte las entidades a DTOs y devuelve
         return products.stream()
                 .map(ClienteConverter::convertToDTO)
                 .collect(Collectors.toList());
     }
 
-    // Método para crear un nuevo producto a partir de un DTO
     @Transactional
     public ClienteDTO createCliente(ClienteDTO clienteDTO) {
-        // Convierte el DTO a entidad
         Cliente cliente = ClienteConverter.convertToEntity(clienteDTO);
-        // Guarda la entidad en la base de datos
         cliente = clienteRepository.save(cliente);
-        // Convierte la entidad guardada a DTO y devuelve
         return ClienteConverter.convertToDTO(cliente);
     }
 
-    // Método para actualizar un producto existente a partir de un DTO
     @Transactional
     public ClienteDTO updateCliente(Long id, Cliente clienteDTO) {
-        // Verifica si el producto existe
         Cliente existingcliente= clienteRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
 
-        // Actualiza los campos del producto existente con los del DTO
         existingcliente.setCodigo(clienteDTO.getCodigo());
         existingcliente.setNombres(clienteDTO.getNombres());
         existingcliente.setApellidos(clienteDTO.getApellidos());
@@ -66,23 +48,17 @@ public class ClienteServices {
         existingcliente.setEdad(clienteDTO.getEdad());
         existingcliente.setTelefono(clienteDTO.getTelefono());
         existingcliente.setFecha_nacimiento(clienteDTO.getFecha_nacimiento());
-        // Puedes actualizar otros campos según sea necesario
 
-        // Guarda la entidad actualizada en la base de datos
         existingcliente = clienteRepository.save(existingcliente);
 
-        // Convierte la entidad actualizada a DTO y devuelve
         return ClienteConverter.convertToDTO(existingcliente);
     }
 
-    // Método para eliminar un producto por ID
     @Transactional
     public void deletecliente(Long id) {
-        // Verifica si el producto existe
         Cliente existingcliente = clienteRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Producto no encontrado"));
 
-        // Elimina el producto del repositorio
         clienteRepository.delete(existingcliente);
     }
 
---
+++ src/main/java/com/example/App/services/UsuarioService.java
@@ -0,0 +1,54 @@
+package com.example.App.services;
+
+import com.example.App.DTO.UsuarioDTO;
+import com.example.App.entities.Usuario;
+import com.example.App.repositories.UsuarioRepository;
+import com.example.App.services.converters.UsuarioConverter;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioService {
+    UsuarioRepository usuarioRepository;
+
+    public List<UsuarioDTO> getAllUsuarios() {
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+        return usuarios.stream().map(usuario -> UsuarioConverter.usuarioToDTO(usuario)).collect(Collectors.toList());
+    }
+
+    public UsuarioDTO getUsuarioById(Long id) {
+        Usuario usuario = usuarioRepository.findById(id).orElse(new Usuario());
+        return UsuarioConverter.usuarioToDTO(usuario);
+    }
+
+    public UsuarioDTO createUsuario(UsuarioDTO usuarioDTO) {
+        Usuario usuario = UsuarioConverter.DTOToUsuario(usuarioDTO);
+        usuario = usuarioRepository.save(usuario);
+        return UsuarioConverter.usuarioToDTO(usuario);
+    }
+
+    public UsuarioDTO updateUsuario(Long id, UsuarioDTO usuarioDTO) {
+        Usuario existingUsuario = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+
+        existingUsuario.setNombre(usuarioDTO.getNombre());
+        existingUsuario.setApellido(usuarioDTO.getApellido());
+        existingUsuario.setPassword(usuarioDTO.getPassword());
+        existingUsuario.setUser_name(usuarioDTO.getUser_name());
+        existingUsuario.setTrabajo(usuarioDTO.getTrabajo());
+        existingUsuario.setEmail(usuarioDTO.getEmail());
+        existingUsuario.setDepartamento(usuarioDTO.getDepartamento());
+
+        existingUsuario = usuarioRepository.save(existingUsuario);
+
+        return UsuarioConverter.usuarioToDTO(existingUsuario);
+    }
+
+    public void deleteUsuario(Long id) {
+        Usuario existingUsuario = usuarioRepository.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+        usuarioRepository.delete(existingUsuario);
+    }
+}
+
