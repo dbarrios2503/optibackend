77e7db21212e8bbccf5ada4b210cdd18f091218c
joshtin2505 <justincastro2505@gmail.com>
2024-05-02 13:32:49 -0500

    Reanamed Service to services

--- src/main/java/com/example/App/services/ClienteConverter.java
+++ src/main/java/com/example/App/services/ClienteConverter.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.example.App.Service;
+package com.example.App.services;
 
 import com.example.App.DTO.ClienteDTO;
 import com.example.App.entities.Cliente;
